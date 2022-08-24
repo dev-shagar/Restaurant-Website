02187681b25b1b40e897a61d44d9be57d4901721
khairul islam shagar <developershagar@gmail.com>
2022-08-24 17:30:34 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+window.sr = ScrollReveal ();
+
+sr.reveal ('.animate-left',{
+    origin: 'left',
+    duration: 1500 ,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal ('.animate-right',{
+    origin: 'right',
+    duration: 1500 ,
+    distance: '25rem',
+    delay: 300
+});
+sr.reveal ('.animate-top',{
+    origin: 'top',
+    duration: 1500 ,
+    distance: '25rem',
+    delay: 700
+});
+
+sr.reveal ('.animate-bottom',{
+    origin: 'bottom',
+    duration: 1500 ,
+    distance: '25rem',
+    delay: 700
+});
+
